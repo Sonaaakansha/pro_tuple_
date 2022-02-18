b96e6227a92175c4b39850c6b4cee720fa028368
Sonaaakansha <99885474+Sonaaakansha@users.noreply.github.com>
2022-02-18 18:19:54 +0530

    Add files via upload

---
+++ tki7.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root=Tk()
+root.geometry()
+f1=Frame(root,bg="gray",borderwidth=6,relief=SUNKEN)
+f1.pack(side=LEFT,)
+l=Label(f1,text="Project Tkinter-Pycharm")
+l.pack(pady=142)
+root.mainloop()
