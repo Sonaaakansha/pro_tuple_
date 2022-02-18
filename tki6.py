7409aafe81cbf53f4d511cd9baa9c4288d4da031
Sonaaakansha <99885474+Sonaaakansha@users.noreply.github.com>
2022-02-18 18:13:23 +0530

    Add files via upload

---
+++ tki6.py
@@ -0,0 +1,11 @@
+from tkinter import *
+imaginary_tech_root= Tk()
+
+imaginary_tech_root.geometry()
+
+#width ,height
+imaginary_tech_root.minsize(200,100)
+imaginary_tech_root.maxsize(1200,988)
+master=Label(text="master is a good boy and this is his GUI")
+
+imaginary_tech_root.mainloop()
